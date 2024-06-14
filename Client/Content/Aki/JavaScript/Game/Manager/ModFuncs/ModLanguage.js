a9b97284dcb0ab36ee875adec5e2f33731561bd7
Robby <harranobu@gmail.com>
2024-06-14 13:43:41 +0800

    fix and add translation

--- Client/Content/Aki/JavaScript/Game/Manager/ModFuncs/ModLanguage.js
+++ Client/Content/Aki/JavaScript/Game/Manager/ModFuncs/ModLanguage.js
@@ -105,92 +105,92 @@ class ModLanguage {
       ja: "オフ",
     },
     {
-      en: "God Mode[F5]",
+      en: "God Mode [F5]",
       chs: "无敌模式[F5]",
       ja: "神モードやで[F5]",
     },
     {
-      en: "Hit Multiplier[F6]",
+      en: "Hit Multiplier [F6]",
       chs: "多倍攻击[F6]",
       ja: "ヒット倍率[F6]",
     },
     {
-      en: "Auto Pick Treasure[F7]",
+      en: "Auto Pick Treasure [F7]",
       chs: "拾取宝箱[F7]",
       ja: "自動で宝を拾うんや[F7]",
     },
     {
-      en: "Auto Absorb[F8]",
+      en: "Auto Absorb [F8]",
       chs: "自动吸收[F8]",
       ja: "自動で吸収すんや[F8]",
     },
     {
-      en: "Kill Aura[F9]",
+      en: "Kill Aura [F9]",
       chs: "杀戮光环[F9]",
       ja: "キルオーラやで[F9]",
     },
     {
-      en: "Perception Range[F10]",
+      en: "Perception Range [F10]",
       chs: "感知范围[F10]",
       ja: "感知範囲やで[F10]",
     },
     {
-      en: "No Cooldown[F11]",
+      en: "No Cooldown [F11]",
       chs: "无冷却[F11]",
       ja: "クールダウンなしやで[F11]",
     },
     {
-      en: "Player Speed[F12]",
+      en: "Player Speed [F12]",
       chs: "玩家速度[F12]",
       ja: "プレイヤーのスピードやで[F12]",
     },
     {
-      en: "CustomTp",
-      chs: "自定义传送",
-      ja: "てきとうな瞬間移動",
+      en: "Custom Teleport [INS]",
+      chs: "自定义传送[INS]",
+      ja: "てきとうな瞬間移動[INS]",
     },
     {
-      en: "Auto Loot[Nun0]",
-      chs: "自动拾取[Nun0]",
-      ja: "自動あっぷる[Nun0]",
+      en: "Auto Loot [Num0]",
+      chs: "自动拾取[Num0]",
+      ja: "自動あっぷる[Num0]",
     },
     {
-      en: "CustomTp State [Insert]:",
+      en: "Custom Teleport State [Insert]:",
       chs: "自定义传送状态[Insert]:",
       ja: "カスタムテレポートの状態[Insert]",
     },
     {
-      en: " Show[Del]",
+      en: " Show [Del]",
       chs: " 显示[Del]",
       ja: " ショー[Del]",
     },
     {
-      en: "CurrentFile:",
+      en: "Current File:",
       chs: "当前文件:",
       ja: "今のファイル",
     },
     {
-      en: "| PreviousFile[PageUp]: ",
+      en: "| PreviousFile[PageUp]:",
       chs: "| 上一个文件[PageUp]:",
       ja: "| 前のファイル[PageUp]",
     },
     {
-      en: " | NextFile[PageDown]: ",
+      en: " | NextFile[PageDown]:",
       chs: " | 下一个文件[PageDown]:",
       ja: " | つぎのファイル[PageDown]",
     },
     {
-      en: " | PreviousPos[Up]: ",
+      en: " | PreviousPos[Up]:",
       chs: " | 上一个位置[Up]:",
       ja: " |前のポジション[Up]",
     },
     {
-      en: " | NextPos[Down]: ",
+      en: " | NextPos[Down]:",
       chs: " | 下一个位置[Down]:",
       ja: " |つぎのポジション[Down]",
     },
     {
-      en: "AntiDither[Num1]",
+      en: "Anti Dither [Num1]",
       chs: "反虚化[Num1]",
       ja: "アンチディザ[Num1]",
     },
@@ -294,10 +294,46 @@ class ModLanguage {
       chs: "自定义UID",
       ja: "カスタムUID",
     },
-
-
-
-  
+    {
+      en: "Player",
+      chs: "玩家",
+      ja: "プレーヤー"
+    },
+    {
+      en: "World",
+      chs: "世界",
+      ja: "世界"
+    },
+    {
+      en: "UI",
+      chs: "用户界面",
+      ja: "インターフェース"
+    },
+    {
+      en: "Hide HUD",
+      chs: "隐藏HUD",
+      ja: "HUDを非表示"
+    },
+    {
+      en: "Hide Damage Text",
+      chs: "隐藏伤害文字",
+      ja: "ダメージテキストを非表示"
+    },
+    {
+      en: "Mark Teleport [T]",
+      chs: "标记传送[T]",
+      ja: "マークテレポート[T]"
+    },
+    {
+      en: "Only Hatred",
+      chs: "只有仇恨",
+      ja: "憎しみだけ"
+    },
+    {
+      en: "Infinity",
+      chs: "无穷",
+      ja: "無限"
+    },
   ];
 
   static ModTr = (string) => {
