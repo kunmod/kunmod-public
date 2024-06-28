b61c65f42ce91df848454473110ab12b161902c4
Robby <harranobu@gmail.com>
2024-06-28 15:18:32 +0800

    Fix SceneItemHitComponent

--- Client/Content/Aki/JavaScript/Game/NewWorld/SceneItem/SceneItemHitComponent.js
+++ Client/Content/Aki/JavaScript/Game/NewWorld/SceneItem/SceneItemHitComponent.js
@@ -19,9 +19,7 @@ var SceneItemHitComponent_1,
             (s = (o < 3 ? r(s) : 3 < o ? r(e, i, s) : r(e, i)) || s);
       return 3 < o && s && Object.defineProperty(e, i, s), s;
     };
-Object.defineProperty(exports, "__esModule", {
-  value: !0,
-}),
+Object.defineProperty(exports, "__esModule", { value: !0 }),
   (exports.SceneItemHitComponent = void 0);
 const UE = require("ue"),
   EntityComponent_1 = require("../../../Core/Entity/EntityComponent"),
@@ -32,13 +30,11 @@ const UE = require("ue"),
   StringUtils_1 = require("../../../Core/Utils/StringUtils"),
   EventDefine_1 = require("../../Common/Event/EventDefine"),
   EventSystem_1 = require("../../Common/Event/EventSystem"),
-  EffectContext_1 = require("../../Effect/EffectContext/EffectContext"),
-  EffectSystem_1 = require("../../Effect/EffectSystem"),
-  GlobalData_1 = require("../../GlobalData"),
   LevelGamePlayController_1 = require("../../LevelGamePlay/LevelGamePlayController"),
   ConfigManager_1 = require("../../Manager/ConfigManager"),
   ModelManager_1 = require("../../Manager/ModelManager"),
   SceneInteractionManager_1 = require("../../Render/Scene/Interaction/SceneInteractionManager"),
+  BulletCollisionUtil_1 = require("../Bullet/BulletStaticMethod/BulletCollisionUtil"),
   BulletUtil_1 = require("../Bullet/BulletUtil"),
   ModManager_1 = require("../../Manager/ModManager"), //add my code
   SceneItemHitUtils_1 = require("./Util/SceneItemHitUtils");
@@ -54,44 +50,44 @@ let SceneItemHitComponent =
   ) {
     constructor() {
       super(...arguments),
-        (this.$Jo = void 0),
-        (this.fte = void 0),
-        (this.a_n = void 0),
-        (this.h_n = new Array()),
-        (this.l_n = new Map()),
-        (this.__n = void 0),
-        (this.u_n = void 0),
-        (this.c_n = void 0),
-        (this.m_n = Rotator_1.Rotator.Create()),
-        (this.i1n = 0);
+        (this.Snn = void 0),
+        (this.Hte = void 0),
+        (this.Wfn = void 0),
+        (this.Kfn = new Array()),
+        (this.Qfn = new Map()),
+        (this.Xfn = void 0),
+        (this.$fn = void 0),
+        (this.Yfn = void 0),
+        (this.Jfn = Rotator_1.Rotator.Create()),
+        (this.efn = 0);
     }
     OnStart() {
       return (
-        (this.$Jo = this.Entity.GetComponent(176)),
-        (this.a_n = this.Entity.GetComponent(114)),
-        (this.fte = this.Entity.GetComponent(181)),
-        (this.i1n = this.Entity.GetComponent(0).GetEntityOnlineInteractType()),
+        (this.Snn = this.Entity.GetComponent(177)),
+        (this.Wfn = this.Entity.GetComponent(115)),
+        (this.Hte = this.Entity.GetComponent(182)),
+        (this.efn = this.Entity.GetComponent(0).GetEntityOnlineInteractType()),
         !0
       );
     }
-    d_n(t) {
-      if (this.$Jo.ContainsTagById(-1431780499)) return !1;
-      if (this.a_n?.IsLocked) return !1;
+    zfn(t) {
+      if (this.Snn.HasTag(-1431780499)) return !1;
+      if (this.Wfn?.IsLocked) return !1;
       if (this.Entity.GetComponent(0)?.IsConcealed) return !1;
       let e = !0;
-      if (0 < this.h_n.length)
-        for (const i of this.h_n) if (!(e &&= i(t))) break;
+      if (0 < this.Kfn.length)
+        for (const i of this.Kfn) if (!(e &&= i(t))) break;
       return e;
     }
-    C_n(t, e) {
-      t = this.l_n.get(t)?.ComponentHitConditionCheck;
+    Zfn(t, e) {
+      t = this.Qfn.get(t)?.ComponentHitConditionCheck;
       if (void 0 === t) return !0;
       let i = !0;
       if (0 < t.length) for (const n of t) if (!(i &&= n(e))) break;
       return i;
     }
-    g_n(t, e) {
-      t = this.l_n.get(t)?.ComponentHitBaseConfig?.HitBullet;
+    epn(t, e) {
+      t = this.Qfn.get(t)?.ComponentHitBaseConfig?.HitBullet;
       return (
         !t ||
         SceneItemHitUtils_1.SceneItemHitUtils.CheckHitDataMatchBulletType(
@@ -101,8 +97,13 @@ let SceneItemHitComponent =
         )
       );
     }
-    OnSceneItemHitOne(t, e) {
-      //copy old
+    OnSceneItemHit(t, e) {
+        if (ModManager_1.ModManager.Settings.HitMultiplier === true) {
+            // for (let j = 0; j < ModManager_1.ModManager.Settings.Hitcount; j++) {
+            //   this.OnSceneItemHitOne(t, e);
+            // }
+            // return ;
+        }
       if (
         (EventSystem_1.EventSystem.EmitWithTarget(
           this.Entity,
@@ -110,7 +111,7 @@ let SceneItemHitComponent =
           t
         ),
         !LevelGamePlayController_1.LevelGamePlayController.MultiplayerLimitTypeCheck(
-          this.i1n,
+          this.efn,
           !1
         ))
       )
@@ -119,12 +120,12 @@ let SceneItemHitComponent =
         t.BulletEntityId
       )?.GetBulletInfo();
       if (i) {
-        if (this.Entity.GetComponent(145)?.ReboundBullet(t, i)) return !1;
+        if (this.Entity.GetComponent(146)?.ReboundBullet(t, i)) return !1;
         if (0 !== t.CalculateType) return !1;
-        if (this.d_n(t)) {
-          this.jOo(t), this.f_n(t, e), this.p_n(t), this.fDo(t);
-          for (var [n] of this.l_n)
-            this.g_n(n, t) && this.C_n(n, t) && this.v_n(n, t);
+        if (this.zfn(t)) {
+          this.c6r(t), this.tpn(t, e), this.ipn(t), this.Lwr(t);
+          for (var [n] of this.Qfn)
+            this.epn(n, t) && this.Zfn(n, t) && this.opn(n, t);
           EventSystem_1.EventSystem.EmitWithTarget(
             this.Entity,
             EventDefine_1.EEventName.OnSceneItemEntityHit
@@ -137,52 +138,40 @@ let SceneItemHitComponent =
       }
       return !0;
     }
-
-    OnSceneItemHit(t, e) {
-  
-      if (ModManager_1.ModManager.Settings.HitMultiplier === true) {
-
-          for (let j = 0; j < ModManager_1.ModManager.Settings.Hitcount; j++) {
-            this.OnSceneItemHitOne(t, e);
-          }
-          return ;
-      }
-      this.OnSceneItemHitOne(t, e);
-    }
     GetPenetrationType() {
       return this.Entity.GetComponent(0).GetBaseInfo().Category
         .BulletPenetrationType;
     }
-    jOo(t) {
-      var e = t.ReBulletData.Render.EffectOnHit.get(4);
-      e &&
-        0 !== e.length &&
-        ((t = new UE.Transform(
+    c6r(t) {
+      var e,
+        i = t.ReBulletData.Render.EffectOnHit.get(4);
+      i &&
+        0 !== i.length &&
+        ((e = new UE.Transform(
           t.HitEffectRotation.ToUeRotator(),
           t.HitPosition.ToUeVector(),
           Vector_1.Vector.OneVector
         )),
-        EffectSystem_1.EffectSystem.SpawnEffect(
-          GlobalData_1.GlobalData.World,
-          t,
+        BulletCollisionUtil_1.BulletCollisionUtil.PlaySceneItemHitEffect(
+          t.Attacker,
+          i,
           e,
-          "[SceneItemHitComponent.ProcessHitEffect]",
-          new EffectContext_1.EffectContext(this.Entity.Id)
+          t.ReBulletData.Render.AudioOnHit
         ));
     }
-    f_n(e, i) {
-      var n = this.Entity.GetComponent(176);
+    tpn(e, i) {
+      var n = this.Entity.GetComponent(177);
       if (n) {
         var r =
           SceneInteractionManager_1.SceneInteractionManager.Get().GetPartCollisionActorsNum(
-            this.fte.GetSceneInteractionLevelHandleId()
+            this.Hte.GetSceneInteractionLevelHandleId()
           );
         let t = void 0;
         r && 0 < r
           ? 1 === i.ValidProcessIndex &&
             (t =
               SceneInteractionManager_1.SceneInteractionManager.Get().GetPartCollisionActorTag(
-                this.fte.GetSceneInteractionLevelHandleId(),
+                this.Hte.GetSceneInteractionLevelHandleId(),
                 i.Actor
               )?.TagId)
           : ((i = (r = EntitySystem_1.EntitySystem.Get(
@@ -192,27 +181,26 @@ let SceneItemHitComponent =
               r.Attacker,
               i
             )),
-            BulletUtil_1.BulletUtil.GetHitRotator(r, this.fte, this.m_n),
+            BulletUtil_1.BulletUtil.GetHitRotator(r, this.Hte, this.Jfn),
             (i = BulletUtil_1.BulletUtil.GetOverrideHitAnimByAngle(
-              this.fte,
+              this.Hte,
               e?.被击动作,
-              this.m_n.Yaw
+              this.Jfn.Yaw
             )),
-            (t = this.M_n(i))),
-          void 0 !== this.c_n &&
-            (n.RemoveTagById(this.c_n), (this.c_n = void 0)),
-          void 0 !== t && (n.AddTagById(t), (this.c_n = t));
+            (t = this.rpn(i))),
+          void 0 !== this.Yfn && (n.RemoveTag(this.Yfn), (this.Yfn = void 0)),
+          void 0 !== t && (n.AddTag(t), (this.Yfn = t));
       }
     }
-    p_n(t) {
+    ipn(t) {
       var e = t.ReBulletData.Base.BeHitEffect,
         e = ConfigManager_1.ConfigManager.BulletConfig.GetBulletHitData(
           t.Attacker,
           e
         );
-      e && this.fte.UpdateHitInfo(t.HitPosition, e.地面受击速度);
+      e && this.Hte.UpdateHitInfo(t.HitPosition, e.地面受击速度);
     }
-    v_n(t, e) {
+    opn(t, e) {
       t?.Valid &&
         (EventSystem_1.EventSystem.EmitWithTarget(
           t,
@@ -224,15 +212,16 @@ let SceneItemHitComponent =
           e
         ));
     }
-    fDo(t) {
-      this.E_n(t), this.S_n(t);
+    Lwr(t) {
+      this.npn(t), this.spn(t);
     }
-    E_n(t) {
+    npn(t) {
       var e,
         i = t.ReBulletData.TimeScale,
         n = i.TimeScaleOnAttack,
-        r = this.__n?.ValueRatio ?? 1,
-        o = this.__n?.TimeRatio ?? 1;
+        r = this.Xfn?.ValueRatio ?? 1,
+        o = this.Xfn?.TimeRatio ?? 1,
+        s = this.Xfn?.MaxExtraTime ?? 0;
       i.TimeScaleOnAttackIgnoreAttacker
         ? 0 < n.时间膨胀时长 &&
           ((e = EntitySystem_1.EntitySystem.Get(
@@ -243,7 +232,7 @@ let SceneItemHitComponent =
             n.优先级,
             n.时间膨胀值 * r,
             n.时间膨胀变化曲线,
-            n.时间膨胀时长 * o,
+            Math.min(n.时间膨胀时长 * o, n.时间膨胀时长 + s),
             1
           ))
         : 0 < n.时间膨胀时长 &&
@@ -251,7 +240,7 @@ let SceneItemHitComponent =
             n.优先级 - 1,
             n.时间膨胀值 * r,
             n.时间膨胀变化曲线,
-            n.时间膨胀时长 * o,
+            Math.min(n.时间膨胀时长 * o, n.时间膨胀时长 + s),
             1
           ),
           (e = i.CharacterCustomKeyTimeScale),
@@ -268,55 +257,56 @@ let SceneItemHitComponent =
                   n.优先级,
                   n.时间膨胀值 * r,
                   n.时间膨胀变化曲线,
-                  n.时间膨胀时长 * o,
+                  Math.min(n.时间膨胀时长 * o, n.时间膨胀时长 + s),
                   1
                 )));
     }
-    S_n(t) {
-      var e, i;
+    spn(t) {
+      var e, i, n;
       t.ReBulletData.Base.ContinuesCollision ||
         ((t = t.ReBulletData.TimeScale.TimeScaleOnHit),
-        (e = this.u_n?.ValueRatio ?? 1),
-        (i = this.u_n?.TimeRatio ?? 1),
+        (e = this.$fn?.ValueRatio ?? 1),
+        (i = this.$fn?.TimeRatio ?? 1),
+        (n = this.$fn?.MaxExtraTime ?? 0),
         0 < t.时间膨胀时长 &&
           this.Entity.GetComponent(107)?.SetTimeScale(
             t.优先级,
             t.时间膨胀值 * e,
             t.时间膨胀变化曲线,
-            t.时间膨胀时长 * i,
+            Math.min(t.时间膨胀时长 * i, t.时间膨胀时长 + n),
             2
           ));
     }
     AddHitCondition(t) {
-      this.h_n.push(t);
+      this.Kfn.push(t);
     }
     RemoveHitCondition(t) {
-      t = this.h_n.indexOf(t);
-      -1 !== t && this.h_n.splice(t, 1);
+      t = this.Kfn.indexOf(t);
+      -1 !== t && this.Kfn.splice(t, 1);
     }
     RegisterComponent(t, e) {
       var i;
-      this.l_n.has(t) ||
+      this.Qfn.has(t) ||
         (((i = new ComponentHitReg()).ComponentHitBaseConfig = e),
-        this.l_n.set(t, i),
+        this.Qfn.set(t, i),
         e?.AttackerHitTimeScaleRatio &&
-          (this.__n = e.AttackerHitTimeScaleRatio),
-        e?.VictimHitTimeScaleRatio && (this.u_n = e.VictimHitTimeScaleRatio));
+          (this.Xfn = e.AttackerHitTimeScaleRatio),
+        e?.VictimHitTimeScaleRatio && (this.$fn = e.VictimHitTimeScaleRatio));
     }
     AddComponentHitCondition(t, e) {
       var i;
-      this.l_n.has(t) || ((i = new ComponentHitReg()), this.l_n.set(t, i)),
-        this.l_n.get(t).ComponentHitConditionCheck.push(e);
+      this.Qfn.has(t) || ((i = new ComponentHitReg()), this.Qfn.set(t, i)),
+        this.Qfn.get(t).ComponentHitConditionCheck.push(e);
     }
     RemoveComponentHitCondition(t, e) {
-      t = this.l_n.get(t)?.ComponentHitConditionCheck;
+      t = this.Qfn.get(t)?.ComponentHitConditionCheck;
       void 0 !== t && -1 !== (e = t.indexOf(e)) && t.splice(e, 1);
     }
-    M_n(t) {
-      return SceneItemHitComponent_1.y_n.get(t);
+    rpn(t) {
+      return SceneItemHitComponent_1.apn.get(t);
     }
   });
-(SceneItemHitComponent.y_n = new Map([
+(SceneItemHitComponent.apn = new Map([
   [8, 631236362],
   [1, -40693742],
   [9, 1688432695],
@@ -328,7 +318,7 @@ let SceneItemHitComponent =
 ])),
   (SceneItemHitComponent = SceneItemHitComponent_1 =
     __decorate(
-      [(0, RegisterComponent_1.RegisterComponent)(137)],
+      [(0, RegisterComponent_1.RegisterComponent)(138)],
       SceneItemHitComponent
     )),
   (exports.SceneItemHitComponent = SceneItemHitComponent);
