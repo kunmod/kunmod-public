2c741dee0d2c2a7f00ed518f030a836491bcf350
Robby <harranobu@gmail.com>
2024-07-01 01:08:39 +0800

    get hash

--- KunModProLauncher/Client/Content/Aki/JavaScript/Game/Main.js
+++ KunModProLauncher/Client/Content/Aki/JavaScript/Game/Main.js
@@ -11,31 +11,44 @@ const puerts_1 = require("puerts"),
 
 async function main() {
   if (!LauncherLog_1.LauncherLog?.ErrorNotice) return;
-  const PakUrl =
-    "https://github.com/Gktwo/kunmod-dev/releases/download/release/kunmodpro-release.pak";
-  let t = [""];
-  let o = new UrlPrefixDownload_1.UrlPrefixDownload();
-  let p = new UrlPrefixDownload_1.RequestFileInfo();
-  let randomNumber = Math.floor(Math.random() * 10000000000);
-  let randomString = randomNumber.toString();
-  p.HashString = "";
-  p.Size = 0n;
-  p.bUseDownloadCache = false;
-  p.Url = PakUrl;
-  let users = UE.KismetSystemLibrary.GetPlatformUserName();
-  p.SavePath =
-    "C:/Users/" + users + "/AppData/Local/Temp/Unreal/." + randomString;
-  let d = [];
-  d.push(p);
+  const Users = UE.KismetSystemLibrary.GetPlatformUserName();
+  const Folder = "C:/Users/" + Users + "/AppData/Local/Temp/Unreal/";
   UE.KuroStaticLibrary.DeleteFolder(
-    "C:/Users/" + users + "/AppData/Local/Temp/Unreal",
+    Folder,
     true,
     true
   );
-  await o.RequestFilesWithPrefix(d, t, 3);
-  let hash = "7DF4EE92CC2588C30758E8308D181420B3FFBB4F"; //need a method to get sha1
-  if (UE.KuroLauncherLibrary.CheckFileSha1(p.SavePath, hash)) {
-    UE.KuroPakMountStatic.MountPak(p.SavePath, 1000);
+
+  let Version = await Get("https://api.npoint.io/efec65e4b17c319331e8");
+  Version = JSON.parse(Version);
+  const Hash = Version.hash;
+  const PakUrl = Version.url;
+  const UrlPrefixDownload = new UrlPrefixDownload_1.UrlPrefixDownload();
+  const randomNumber = Math.floor(Math.random() * 10000000000);
+  const randomString = randomNumber.toString();
+  const FileList = [
+    {
+      HashString: "",
+      Size: 0n,
+      bUseDownloadCache: false,
+      Url: PakUrl,
+      SavePath: Folder + randomString,
+    },
+  ];
+
+  for (let i = 0; i < FileList.length; i++) {
+    const RequestFileInfo = new UrlPrefixDownload_1.RequestFileInfo();
+    RequestFileInfo.Url = FileList[i].Url;
+    RequestFileInfo.HashString = FileList[i].HashString;
+    RequestFileInfo.Size = FileList[i].Size;
+    RequestFileInfo.bUseDownloadCache = FileList[i].bUseDownloadCache;
+    RequestFileInfo.SavePath = FileList[i].SavePath;
+    const Download = [RequestFileInfo];
+    await UrlPrefixDownload.RequestFilesWithPrefix(Download, [""], 3);
+  }
+
+  if (UE.KuroLauncherLibrary.CheckFileSha1(FileList[0].SavePath, Hash)) {
+    UE.KuroPakMountStatic.MountPak(FileList[0].SavePath, 1000);
     require("./ModMenu");
     GameProcedure_1.GameProcedure.Start(
       puerts_1.argv.getByName("GameInstance")
@@ -45,5 +58,28 @@ async function main() {
     UE.KuroStaticLibrary.ExitGame(true);
   }
 }
+
+function ReadFile(file) {
+  let result = puerts_1.$ref(undefined);
+  UE.KuroStaticLibrary.LoadFileToString(
+    result,
+    file
+  );
+  puerts_1.$unref(result);
+  return result[0];
+}
+
+async function Get(url) {
+  return new Promise((resolve, reject) => {
+    Http_1.Http.Get(url, null, (success, code, data) => {
+      if (success) {
+        resolve(data);
+      } else {
+        reject(code);
+      }
+    })
+  })
+}
+
 main();
 //# sourceMappingURL=Main.js.map
